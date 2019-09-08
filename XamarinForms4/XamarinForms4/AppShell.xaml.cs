f67eae218dc644d7636cd24c2261c4f5b637214a
jorgemhtdev <jorge@jorgemht.dev>
2019-09-08 21:30:19 +0200

    First commit

---
+++ XamarinForms4/XamarinForms4/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace XamarinForms4
+{
+    using Xamarin.Forms;
+
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamarinForms4/XamarinForms4/Views/SecondaryView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinForms4.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SecondaryView : ContentPage
+    {
+        public SecondaryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
